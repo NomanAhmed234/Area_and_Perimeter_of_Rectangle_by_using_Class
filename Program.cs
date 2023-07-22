40b4629763a0202043353e3932618bee3673835b
Noman Ahmed <nomanpb234@gmail.com>
2023-07-22 23:22:53 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*Create a rectangle class with two data members’ length and width Provide member
+functions to calculate the perimeter and area of the rectangle and a function square
+which returns true if the rectangle is a square otherwise it returns false.*/
+
+namespace Lab_5q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.area(6,7);
+            rect.perimeter(6,6);
+            Console.WriteLine(rect.square());
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5q4
+{
+    internal class Rectangle
+    {
+        public int width;
+        public int length;
+
+        public void perimeter(int w, int l)
+        {
+            this.width = w;
+            this.length = l;
+            int perimeter = (2 * width) + (2 * length);
+            Console.WriteLine("The Perimeter of Rectangle is: {0}.",perimeter);
+        }
+
+        public void area(int w, int l)
+        {
+            this.width = w;
+            this.length = l;
+            int area = width * length;
+            Console.WriteLine("The Area of Rectangle is: {0}.",area);
+        }
+
+        public bool square()
+        {
+            if (length == width)
+            {
+                return true;
+            }else { return false; }
+        }
+    }
+}
